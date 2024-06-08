f28d08cdaed5344cbc99fa9a77ed8e17a09962d0
Huỳnh Ngọc Ý Nhi <21522423@gm.uit.edu.vn>
2024-06-08 10:10:48 +0700

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestHttp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Get thành công");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] TestData data)
+        {
+            if (data == null || string.IsNullOrEmpty(data.Value))
+            {
+                return BadRequest("Invalid data.");
+            }
+            return Ok($"Received: {data.Value}");
+        }
+
+    }
+    public class TestData
+    {
+        public string Value { get; set; }
+    }
+}
